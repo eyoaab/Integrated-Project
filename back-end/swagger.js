d9278f66840615f52263198a6bc6a9b7d27ebeb5
eyoaab <eyobtariku48@gmail.com>
2025-05-11 13:57:38 +0300

    improve  the swagger of the website

--- back-end/swagger.js
+++ back-end/swagger.js
@@ -18,7 +18,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: "http://localhost:3000",
+      url: "https://integrated-project-mf1f.onrender.com",
       description: "Development Server",
     },
   ],
